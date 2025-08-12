dc255f62f9da8d98905cfe64f5a539a672718227
BishopBatman <theprawn26@gmail.com>
2025-08-12 19:33:55 +0100

    Rename Dopewars references

--- src/gui_client/newgamedia.c
+++ src/gui_client/newgamedia.c
@@ -2,7 +2,7 @@
  * newgamedia.c   New game dialog                                       *
  * Copyright (C)  1998-2022  Ben Webb                                   *
  *                Email: benwebb@users.sf.net                           *
- *                WWW: https://dopewars.sourceforge.io/                 *
+ *                WWW: https://churchwars.sourceforge.io/               *
  *                                                                      *
  * This program is free software; you can redistribute it and/or        *
  * modify it under the terms of the GNU General Public License          *
@@ -93,7 +93,7 @@ static void ConnectError(void)
     g_string_assign(neterr, _("Connection closed by remote host"));
   }
 
-  /* Error: GTK+ client could not connect to the given dopewars server */
+  /* Error: GTK+ client could not connect to the given Church Wars server */
   text = g_strdup_printf(_("Status: Could not connect (%s)"), neterr->str);
 
   SetStartGameStatus(text);
