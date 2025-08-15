8ee99ffe7e15ec470259a631cfd112ca2edd3a0c
BishopBatman <theprawn26@gmail.com>
2025-08-15 15:22:41 +0100

    audio: launcher to run churchwars with stable ALSA settings

--- scripts/run_churchwars.sh
+++ scripts/run_churchwars.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 export SDL_AUDIODRIVER=alsa
 export SDL_ALSA_PCM_DEVICE=default
-export AUDIODEV=default
+export SDL_AUDIO_ALSA_SET_BUFFER_SIZE=16384
 exec "$(dirname "$0")/../src/churchwars" "$@"
