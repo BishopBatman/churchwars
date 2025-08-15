30a374b5b61ec9c4ab06236c4dfe5ef877c720ee
BishopBatman <theprawn26@gmail.com>
2025-08-15 10:15:54 +0100

    sound: take origin/vm-audio-stability version after last night's work

--- src/sound.c
+++ src/sound.c
@@ -62,7 +62,6 @@ gchar *GetPluginList(void)
 {
   GSList *listpt;
   GString *plugins;
-  gchar *retstr;
 
   plugins = g_string_new("\""NOPLUGIN"\"");
   for (listpt = driverlist; listpt; listpt = g_slist_next(listpt)) {
@@ -72,9 +71,7 @@ gchar *GetPluginList(void)
       g_string_append_printf(plugins, ", \"%s\"", drivpt->name);
     }
   }
-  retstr = plugins->str;
-  g_string_free(plugins, FALSE);
-  return retstr;
+  return g_string_free(plugins, FALSE);
 }
 
 static void AddPlugin(InitFunc ifunc, void *module)
@@ -212,12 +209,13 @@ TryLoadPlugin(const gchar *name)
   driver = GetPlugin(name);
   if (!driver) {
     if (name) {
-      fprintf(stderr, "Sound plugin '%s' not found\n", name);
+      fprintf(stderr, "Sound plugin '%s' not found; falling back.\n", name);
     }
     return FALSE;
   }
   if (driver->open && !driver->open()) {
-    fprintf(stderr, "Sound plugin '%s' failed to initialize\n", name);
+    fprintf(stderr, "Sound plugin '%s' failed to initialize; falling back.\n",
+            name);
     driver = NULL;
     return FALSE;
   }
@@ -232,25 +230,13 @@ void SoundOpen(gchar *drivername)
   sound_enabled = FALSE;
 
   envplug = g_getenv("CHURCHWARS_SOUND_PLUGIN");
-  if (envplug) {
-    gchar *envnorm = g_ascii_strdown(envplug, -1);
-    if (TryLoadPlugin(envnorm)) {
-      g_free(envnorm);
-      return;
-    }
-    if (envnorm[0]) {
-      fprintf(stderr, "Falling back from CHURCHWARS_SOUND_PLUGIN '%s'\n", envplug);
-    }
-    g_free(envnorm);
+  if (envplug && envplug[0] && TryLoadPlugin(envplug)) {
+    return;
   }
 
-  if (drivername) {
-    gchar *drivernorm = g_ascii_strdown(drivername, -1);
-    if (strcmp(drivernorm, NOPLUGIN) != 0 && TryLoadPlugin(drivernorm)) {
-      g_free(drivernorm);
-      return;
-    }
-    g_free(drivernorm);
+  if (drivername && strcmp(drivername, NOPLUGIN) != 0 &&
+      TryLoadPlugin(drivername)) {
+    return;
   }
 
 #ifdef _WIN32
@@ -258,14 +244,19 @@ void SoundOpen(gchar *drivername)
     return;
   }
 #else
+#if defined(__APPLE__) || defined(HAVE_COCOA)
+  if (TryLoadPlugin("cocoa")) {
+    return;
+  }
+#endif
   if (TryLoadPlugin("sdl")) {
     return;
   }
 #endif
 
   driver = GetPlugin(NULL);
-  if (driver && TryLoadPlugin(driver->name)) {
-    return;
+  if (driver) {
+    TryLoadPlugin(driver->name);
   }
 }
 
